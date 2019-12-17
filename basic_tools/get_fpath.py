c84aad8dbe19583ddc140dbe2bc3a3fb92b021ad
Todd <bh_song@163.com>
2019-12-17 10:37:45 +0800

    update processbar and add processbar for multi-task

--- basic_tools/get_fpath.py
+++ basic_tools/get_fpath.py
@@ -1,28 +1,34 @@
 import os
 import re
 
-def get_fpath(dir,suffix,pattern=None,is_exclude=False):
+def get_fpath(dir,suffix,pattern=None,is_exclude=False,is_absolute=False):
     """
     """
     suffix_len = len(suffix)
     file_filter = lambda fname:(len(fname)>suffix_len and
                                      fname[-suffix_len:] == suffix and
                                      fname[0]!='.')
-    fpath_all = []
+    fpath_relative_all = []
     for root,dirs,files in os.walk(dir):
         for fname in files:
             if not file_filter(fname):
                 continue
-            fpath_relative = os.path.relpath(os.path.join(root,fname),dir)
+
+            fpath_relative = os.path.relpath(fpath_absolute,dir)
             if pattern is not None:
                 n_match = len(re.findall(pattern,fpath_relative))
                 if is_exclude and n_match>0:
                     continue
                 if (not is_exclude) and n_match<1:
                     continue
-            fpath_all.append(fpath_relative)
+            fpath_relative_all.append(fpath_relative)
 
-    return fpath_all
+    if is_absolute:
+        fpath_absolute = [os.path.join(dir,fpath_relative)
+                             for fpath_relative in fpath_relative_all]
+        return fpath_absolute
+    else:
+        return fpath_relative_all
 
 
 if __name__ == '__main__':
