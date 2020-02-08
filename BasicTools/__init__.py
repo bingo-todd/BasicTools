3efb78099bce42c9a46f575009affb922ac34c7b
Todd <bh_song@163.com>
2020-02-08 22:27:10 +0800

    import module in __init__

--- BasicTools/__init__.py
+++ BasicTools/__init__.py
@@ -2,3 +2,4 @@ from .import Filter_GPU, ProcessBarMulti, TFData, fft, gcc
 from . import reverb_time, wav_tools, unit_convert, query_resrc
 from .get_fpath import get_fpath
 from .ProcessBar import ProcessBar
+from .ProcessBarMulti import ProcessBarMulti
